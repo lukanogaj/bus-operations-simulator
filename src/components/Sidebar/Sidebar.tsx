a385db38886f3e1c88916c0ae9e7b725e0b04c5d
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-07-07 09:35:37 +0100

    build sidebar navigation

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,33 @@
 import styles from "./Sidebar.module.css";
 
+const navigationItems = [
+	"Dashboard",
+	"Drivers",
+	"Duties",
+	"Vehicles",
+	"Allocation",
+	"Operations Board",
+	"Incidents",
+	"Reports",
+	"Admin",
+];
+
 const Sidebar = () => {
-	return <aside className={styles.sidebarShell}>Sidebar</aside>;
+	return (
+		<aside className={styles.sidebarShell}>
+			<nav>
+				<ul className={styles.navigationList}>
+					{navigationItems.map((item) => (
+						<li
+							key={item}
+							className={styles.navigationItem}>
+							{item}
+						</li>
+					))}
+				</ul>
+			</nav>
+		</aside>
+	);
 };
 
 export default Sidebar;
