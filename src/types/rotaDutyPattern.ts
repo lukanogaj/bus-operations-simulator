58e3dcbaf9ed200fa2f3f8b55d633f3f8563340b
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-08-05 09:24:14 +0100

    feat: add RestDayPattern interface

---
+++ src/types/rotaDutyPattern.ts
@@ -0,0 +1,10 @@
+export interface RestDayPattern {
+	patternNumber: number;
+	saturday: number | "R";
+	sunday: number | "R";
+	monday: number | "R";
+	tuesday: number | "R";
+	wednesday: number | "R";
+	thursday: number | "R";
+	friday: number | "R";
+}
